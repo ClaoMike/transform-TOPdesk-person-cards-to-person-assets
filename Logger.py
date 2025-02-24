756368b2d4cd046594f959a4d4444a6fe352f114
Claudiu Mihai Jechel <jechelclaudiumihai@gmail.com>
2025-02-24 11:03:36 +0100

    Put each log into its own folder

--- Logger.py
+++ Logger.py
@@ -5,11 +5,17 @@ import os
 class Logger:
     def __init__(self, log_dir="logs"):
         # if the logs directory does not exist, we create it
-        os.makedirs(log_dir, exist_ok=True)
+        self.log_dir = log_dir
+
+        os.makedirs(self.log_dir, exist_ok=True)
+
+        run_timestamp = datetime.now().strftime("app_%Y-%m-%d_%H-%M-%S")
+        self.log_dir = os.path.join(self.log_dir, run_timestamp)
+        os.makedirs(self.log_dir, exist_ok=True)
 
         # Generate a timestamped filename
-        log_filename = datetime.now().strftime("app_%Y-%m-%d_%H-%M-%S.log")
-        log_path = os.path.join(log_dir, log_filename)
+        log_filename = datetime.now().strftime(f"{run_timestamp}.log")
+        log_path = os.path.join(self.log_dir, log_filename)
 
         # Configure logging
         logging.basicConfig(
